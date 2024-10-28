353d93afc588c1e3df55430074c05cd99dab1428
Alistair White-Horne <42572235+dapeef@users.noreply.github.com>
2024-10-28 12:41:58 +0000

    Improve braille module

--- braille_utils.py
+++ braille_utils.py
@@ -2,6 +2,7 @@ import numpy as np
 
 ascii_chars = " A1B'K2L@CIF/MSP\"E3H9O6R^DJG>NTQ,*5<-U8V.%[$+X!&;:4\\0Z7(_?W]#Y)=".lower()
 braille_chars = "⠀⠁⠂⠃⠄⠅⠆⠇⠈⠉⠊⠋⠌⠍⠎⠏⠐⠑⠒⠓⠔⠕⠖⠗⠘⠙⠚⠛⠜⠝⠞⠟⠠⠡⠢⠣⠤⠥⠦⠧⠨⠩⠪⠫⠬⠭⠮⠯⠰⠱⠲⠳⠴⠵⠶⠷⠸⠹⠺⠻⠼⠽⠾⠿"
+numbers = "0123456789"
 
 
 class BrailleConfig:
@@ -17,63 +18,68 @@ class BrailleConfig:
     INCLUDE_CAPS = True # Should the braille display capital letters, or should they all be lower
 
 
-def str_to_braille(string:str) -> str:
+def str_to_braille(string:str, legible:bool=False) -> str:
     braille_string = ""
+    was_previous_number = False
 
     for char in string:
         if char.lower() in ascii_chars:
-            # Handle capital letters
+            # Handle prefix characters
             if char.isupper():
-                braille_string += braille_chars[ascii_chars.index(",")] + " "
-            
+                braille_string += braille_chars[ascii_chars.index(",")]
+
+            if char in numbers:
+                if not was_previous_number:
+                    braille_string += braille_chars[ascii_chars.index("#")]
+                was_previous_number = True
+            else:
+                was_previous_number = False
+
             # Handle letter
-            braille_string += braille_chars[ascii_chars.index(char.lower())] + " "
+            braille_string += braille_chars[ascii_chars.index(char.lower())]
 
         else:
             raise Exception(f"Ascii character \"{char}\" can't be converted to braille.")
+    
+    if legible:
+        legible_string = ""
+        for char in braille_string:
+            legible_string += char + " "
+        
+        braille_string = legible_string[:-1]
+
         
     return braille_string
 
-def char_to_dots(char:str, position:np.ndarray, config:BrailleConfig) -> np.ndarray:
+def char_to_dots(braille_char:str, position:np.ndarray, config:BrailleConfig) -> np.ndarray:
     points = []
 
-    char = char.lower()
+    binary_string = f"{braille_chars.index(braille_char):06b}" # eg 5 -> 000101
+    binary_string = binary_string[::-1] # Get lsb first
 
-    if char in ascii_chars:
-        binary_string = f"{ascii_chars.index(char):06b}" # eg 5 -> 000101
-        binary_string = binary_string[::-1] # Get lsb first
-
-        for i in range(6):
-            if binary_string[i] == "1":
-                points.append(np.array([
-                    np.floor_divide(i, 3),
-                    -(i % 3),
-                    0]) * config.DOT_SPACING + position)
-
-    else:
-        raise Exception(f"Ascii character \"{char}\" can't be converted to braille.")
+    for i in range(6):
+        if binary_string[i] == "1":
+            points.append(np.array([
+                np.floor_divide(i, 3),
+                -(i % 3),
+                0]) * config.DOT_SPACING + position)
 
     return points
 
 def str_to_dots(string:str,
-                start_position:np.ndarray=np.array([0, 0, 0]),
-                rotation:float=0,
                 config:BrailleConfig=BrailleConfig()) -> np.ndarray:
     points = []
+    position = np.array([0, 0, 0])
 
-    position = start_position.copy()
+    braille_string = str_to_braille(string)
 
-    for char in string:
+
+    for char in braille_string:
         if char == "\n":
-            position[0] = start_position[0]
+            position[0] = 0
             position[1] += config.CELL_SPACING_Y
 
         else:
-            # Handle capital letters
-            if char.isupper() and config.INCLUDE_CAPS:
-                points += char_to_dots(",", position, config)
-                position[0] += config.CELL_SPACING_X
-
             # Add letter
             points += char_to_dots(char, position, config)
             position[0] += config.CELL_SPACING_X
@@ -84,7 +90,7 @@ def str_to_dots(string:str,
 if __name__ == "__main__":
     from geometry_utils import plot_3d_path
 
-    string = "Cambridge Surprise Major"
+    string = "Cambridge 888"
 
-    print(str_to_braille(string))
+    print(str_to_braille(string, legible=True))
     plot_3d_path(str_to_dots(string))
